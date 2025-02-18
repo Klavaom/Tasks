6a59b097bb7c525417f7bea9ffc7f45f75f37543
Klavaom <6705456@mail.ru>
2025-02-18 20:08:36 +0300

    Update 4 task.py

--- 4 task.py
+++ 4 task.py
@@ -1,8 +1,8 @@
 import csv, random
 
 name = "board_id.csv"
+length_sort = []
 length = []
-
 
 def randomaise(first, end):
     num = random.randrange(first, end)
@@ -22,44 +22,43 @@ def read(name):
     with open(name, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
+            length_sort.append(int(row[" Length"]))
             length.append(int(row[" Length"]))
-        length.sort()
+        length_sort.sort()
 
 def max():
-    max=length[999]
-    with open(name, newline="") as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            if row[" Length"] == str(max):
-                id = row["Board_id"]
-                
+    max=length_sort[999]
+    index = 0
+    for i in range(999):      
+        index += 1
+        if length[index] == max:
+            id = index
     print(id, max) 
     return(id, max)
 
 def low():
-    low=length[0]
-    with open(name, newline="") as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            if row[" Length"] == str(low):
-                id = row["Board_id"]
+    low=length_sort[0]
+    index = 0
+    for i in range(999):
+        index += 1
+        if length[index] == low:
+            id = index
+
     print(id, low)
     return(id, low)
 
 def midlle():
     summ = 0
     index = 0
-    for i in length:
-        summ += length[index]
+    for i in length_sort:
+        summ += length_sort[index]
         index += 1
     midlle = summ / 1000
     print(midlle)
     return(midlle)
+
 new_file(name)
 read(name)
-
-
-
 
 
 
